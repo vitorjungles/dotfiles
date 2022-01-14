1f8d77aced806e613aece37742a73f51b879dd53
Vítor Jungles <74316093+vitorjungles@users.noreply.github.com>
2022-01-14 19:42:07 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-sudo apt install build-essential default-jdk libssl-dev exuberant-ctags ncurses-term ack-grep silverseacher-ag fontconfig imagemagick libmagickwand-dev software properties-common git vim-gtk3 curl -y
+sudo apt install build-essential default-jdk libssl-dev exuberant-ctags ncurses-term ack-grep silversearcher-ag fontconfig imagemagick libmagickwand-dev software-properties-common git vim-gtk3 curl -y
