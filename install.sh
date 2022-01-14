766004a758a76d40fdd001f7545c50df87246e91
Vítor Jungles <74316093+vitorjungles@users.noreply.github.com>
2022-01-14 19:39:08 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+sudo apt install build-essential default-jdk libssl-dev exuberant-ctags ncurses-term ack-grep silverseacher-ag fontconfig imagemagick libmagickwand-dev software properties-common git vim-gtk3 curl -y
