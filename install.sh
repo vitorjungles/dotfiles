0fd27e1bf383c4b04e99531bfe868b694335c128
Vítor Jungles <74316093+vitorjungles@users.noreply.github.com>
2022-01-14 19:55:14 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
 sudo apt install build-essential default-jdk libssl-dev exuberant-ctags ncurses-term ack-grep silversearcher-ag fontconfig imagemagick libmagickwand-dev software-properties-common git vim-gtk3 curl -y
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0
+
+echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
+
+echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
+
